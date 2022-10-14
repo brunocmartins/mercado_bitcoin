b6a0d64f96a717b1d4c1bb96fae1758ab37242b8
Bruno Martins <brunocmartins4@gmail.com>
2022-10-14 19:50:54 -0300

    create ingestion method for day-summary

---
+++ mercado_bitcoin/main.py
@@ -0,0 +1,26 @@
+import datetime
+
+from schedule import repeat, every, run_pending
+import time
+
+from ingestors import DaySummaryIngestor, TradesIngestor
+from writers import DataWriter
+
+
+if __name__ == "__main__":
+    day_summary_ingestor = DaySummaryIngestor(
+        writer=DataWriter,
+        coins=["BTC", "ETH", "LTC"],
+        default_start_date=datetime.date(2022, 10, 2),
+    )
+
+    trades_ingestor = TradesIngestor()
+
+    @repeat(every(1).seconds)
+    def job():
+        day_summary_ingestor.ingest()
+        trades_ingestor.ingest()
+
+    while True:
+        run_pending()
+        time.sleep(0.5)
